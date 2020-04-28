fc7f274aeccd608ca076420d53e2039a64b023c7
bigLucas <contato.lferreira@hotmail.com>
2020-04-27 22:45:47 -0300

    added class FlashMemoryHex

--- app/src/main/java/com/example/tcc_pro_v02/Compiler/Compiler.java
+++ app/src/main/java/com/example/tcc_pro_v02/Compiler/Compiler.java
@@ -3,6 +3,7 @@ package com.example.tcc_pro_v02.Compiler;
 import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Compiler {
@@ -15,9 +16,7 @@ public class Compiler {
         this.logic = logic;
     }
 
-    // TODO: terminar o compilador, ainda estou enviando um hex mocado
-    // posso partir do mock e criar outros aqrquivos só mudando
-    // a entrada que aciona a saída
+    // TODO: terminar o compilador, continuar debug, algo de errado no hexafiles
 
     public int[] compileLogic() {
         int[] logicCompiled = new int[]{};
@@ -44,14 +43,10 @@ public class Compiler {
             }
             System.out.println("DEBUG " + auxInPoints.toString());
             System.out.println("DEBUG " + auxLinePoints.toString());
-            List<Integer> aux = new ArrayList<>();
-            aux.add(1);
-            aux.add(2);
-            aux.add(3);
-            aux.add(4);
-            if (auxInPoints.contains(0) && auxLinePoints.containsAll(aux)) {
-                logicCompiled = HexFiles.IN_0_OUT_0;
-            }
+            logicCompiled = getHexFile(0,0, outPoint, auxInPoints.get(0), auxLinePoints);
+            int[] logic = getHexFile(1,0, outPoint, auxInPoints.get(0), auxLinePoints);
+            System.out.println("DEBUG " + logic.length);
+            System.out.println("DEBUG " + Arrays.equals(logicCompiled, logic));
         }
         return logicCompiled;
     }
@@ -84,11 +79,24 @@ public class Compiler {
         return points;
     }
 
-//    private void findConnections(String[] out) {
-//        if(out.length > 0) {
-//            for (String point: out) {
-//
-//            }
-//        }
-//    }
+    private int[] getHexFile(int outputPointInPLC, int inputPointInPLC, int outputSlot, int inputSlot, List<Integer> linesList) {
+        final int OUT_0 = 0;
+        final int OUT_1 = 1;
+        final int IN_0 = 0;
+        final int IN_1 = 1;
+        final int IN_2 = 2;
+        final int IN_3 = 3;
+        final int SLOT_OUT_5 = 5;
+        final int SLOT_OUT_11 = 11;
+        final int SLOT_OUT_17 = 17;
+        final int SLOT_OUT_23 = 23;
+        final int SLOT_OUT_29 = 29;
+        if (outputPointInPLC == OUT_0 && inputPointInPLC == IN_0 && outputSlot == SLOT_OUT_5 && inputSlot == 0 && linesList.containsAll(Arrays.asList(1,2,3,4))) {
+            return HexFiles.BASE_IN_0_OUT_0;
+        }
+        if (outputPointInPLC == OUT_1 && inputPointInPLC == IN_0 && outputSlot == SLOT_OUT_5 && inputSlot == 0 && linesList.containsAll(Arrays.asList(1,2,3,4))) {
+            return HexFiles.setNewHexFile(HexFiles.IN_1_OUT_0, HexFiles.BASE_IN_0_OUT_0);
+        }
+        return HexFiles.BASE_IN_0_OUT_0;
+    }
 }
---
+++ app/src/main/java/com/example/tcc_pro_v02/Compiler/FlashMemoryHex.java
@@ -0,0 +1,11 @@
+package com.example.tcc_pro_v02.Compiler;
+
+public class FlashMemoryHex {
+    public int index;
+    public int hex;
+
+    FlashMemoryHex(int index, int hex) {
+        this.hex = hex;
+        this.index =index;
+    }
+}
